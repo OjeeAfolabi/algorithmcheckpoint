a69d83cdfcb62b54b7dcd95f5a63035204962131
OjeeAfolabi <afolabiojee@gmail.com>
2024-02-17 13:25:32 +0100

    length of words in a sentence

--- algocp.js
+++ algocp.js
@@ -1,3 +1,11 @@
 let words="MY NAME IS OJAY";
 let length= words.length;
 console.log(length)
+
+
+// length of words in a sentence
+
+let sentence = "I AM A FULLSTACK JS STUDENT";
+let word = sentence.trim;
+let wordOne = sentence.split(/\s+/);
+console.log(wordOne.length);
